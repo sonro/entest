12bf8bce4ae11d9bbf8e2be1af8923e5ea0fdc03
Christopher Morton <sonro@gmx.com>
2021-10-13 14:31:16 +0100

    Refactor ClassTesterBuilder tests

--- tests/ClassTesterBuilderTest.php
+++ tests/ClassTesterBuilderTest.php
@@ -12,29 +12,31 @@ class ClassTestererBuilderTest extends TestCase
 {
     public function test_exists(): void
     {
-        $builder = new ClassTesterBuilder("ClassName");
+        $builder = $this->createClassTesterBuilder();
         $this->assertInstanceOf(ClassTesterBuilder::class, $builder);
     }
 
     public function test_build_class_tester(): void
     {
-        $builder = new ClassTesterBuilder("ClassName");
+        $builder = $this->createClassTesterBuilder();
         $classTester = $builder->build();
         $this->assertInstanceOf(ClassTester::class, $classTester);
     }
 
     public function test_class_name_used_in_build(): void
     {
-        $className = "ClassName";
-        $builder = new ClassTesterBuilder($className);
-        $classTester = $builder->build();
-        $actual = $classTester->getClassName();
-        $this->assertEquals($className, $actual);
+        $classNames = ["ClassName", "AnotherClassName"];
+        foreach ($classNames as $className) {
+            $builder = $this->createClassTesterBuilder(className: $className);
+            $classTester = $builder->build();
+            $actual = $classTester->getClassName();
+            $this->assertEquals($className, $actual);
+        }
     }
 
     public function test_add_prop_test_chainable(): void
     {
-        $builder = new ClassTesterBuilder("ClassName");
+        $builder = $this->createClassTesterBuilder();
         $propTester = $this->getDummyPropTester();
         $result = $builder->addPropTester($propTester);
         $this->assertEquals($result, $builder);
@@ -42,12 +44,20 @@ class ClassTestererBuilderTest extends TestCase
 
     public function test_add_prop_test_used_in_build(): void
     {
-        $builder = new ClassTesterBuilder("ClassName");
+        $builder = $this->createClassTesterBuilder();
         $propTester = $this->getDummyPropTester();
         $builder->addPropTester($propTester);
         $classTester = $builder->build();
         $propTesters = $classTester->getPropTesters();
         $this->assertContains($propTester, $propTesters);
+    }
+
+    private function createClassTesterBuilder(
+        string $className = null,
+    ): ClassTesterBuilder {
+        return new ClassTesterBuilder(
+            className: $className ?? "DefaultClassName",
+        );
     }
 
     /**
